d73ba57e9a2ec8bd5e510f1d5ee99c6ffde27fa3
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-22 00:23:37 -0500

    Adding answers to jest assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,83 @@
-export function hello() {
-    return "hello"
+export function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+export function reverseWord(string) {
+    let reversedWord = ""
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedWord += string[i]
+    }
+    return reversedWord
+}
+
+export function calculate(operation, int1, int2) {
+    if (operation === "add") {
+        return int1 + int2
+    }
+    if (operation === "subtract") {
+        return int1 - int2
+    }
+    if (operation === "multiply") {
+        return int1 * int2
+    }
+    if (operation === "divide") {
+        return int1 / int2
+    }
+}
+
+export function caesarCipher(string, shiftFactor) {
+    let adjustedWord = ""
+    let shiftAmount = shiftFactor
+    const punctuationArray = [
+        '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+        ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
+    ]
+    if (shiftAmount > 26) {
+        shiftAmount -= 26
+    }
+
+    if (shiftAmount === 0) {
+        return string
+    }
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === " " || punctuationArray.includes(string[i])) {
+            adjustedWord += string[i]
+            continue
+        }
+
+        if (string[i] === string[i].toUpperCase()) {
+            let currentAscii = string[i].charCodeAt(0)
+            let newAscii = (currentAscii + shiftAmount) > 90 ? (currentAscii - 90 + shiftAmount) + 64 : currentAscii + shiftAmount
+            adjustedWord += String.fromCharCode(newAscii)
+        }
+        else {
+            let currentAscii = string[i].charCodeAt(0)
+            let newAscii = (currentAscii + shiftAmount) > 122 ? (currentAscii - 122 + shiftAmount) + 96 : currentAscii + shiftAmount
+            adjustedWord += String.fromCharCode(newAscii)
+        }
+
+    }
+    return adjustedWord
+}
+
+export function analyzeArray(array) {
+    let obj = {}
+    let average = 0
+    let min = Infinity
+    let max = -Infinity
+    for (let i = 0; i < array.length; i++) {
+        average += array[i]
+        if (array[i] > max) {
+            max = array[i]
+        }
+        if (array[i] < min) {
+            min = array[i]
+        }
+    }
+    obj.average = average / array.length
+    obj.min = min
+    obj.max = max
+    obj.length = array.length
+    return obj
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,42 @@
-import { hello } from "./index.js"
+import { analyzeArray, caesarCipher, calculate, capitalize, reverseWord } from "./index.js"
 
-test("Says hello", () => {
-    expect(hello()).toBe("hello")
+test ("Capitalizes `zebra`", () => {
+    expect(capitalize("zebra")).toBe("Zebra")
+})
+
+test ("Reverses `Zebra`", () => {
+    expect(reverseWord("Zebra")).toBe("arbeZ")
+})
+
+test ("Calculate Adds", () => {
+    expect(calculate('add',2,3)).toBe(5)
+})
+
+test ("Calculate Subtracts", () => {
+    expect(calculate('subtract',2,3)).toBe(-1)
+})
+
+test ("Calculate Multiplies", () => {
+    expect(calculate('multiply',2,3)).toBe(6)
+})
+
+test ("Calculate Divides", () => {
+    expect(calculate('divide',12,3)).toBe(4)
+})
+
+test ("Caesar Cipher Works for Hamburger shifted 30", () => {
+    expect(caesarCipher('Hamburger', 30)).toBe('Leqfyvkiv')
+})
+
+test ("Caesar Cipher Works for zZzZz shifted 5", () => {
+    expect(caesarCipher('zZzZz', 5)).toBe('eEeEe')
+})
+
+test ("Caesar Cipher Works for 'Yo Man What's Up? Today is so ... NICE!' shifted 5", () => {
+    expect(caesarCipher(`Yo Man What's Up? Today is so ... NICE!`, 5)).toBe(`Dt Rfs Bmfy'x Zu? Ytifd nx xt ... SNHJ!`)
+})
+
+const answerObj = { average: 4, min: 1, max: 8, length: 6 };
+test("Analyze Array works for [1,8,3,4,2,6]", () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual(answerObj)
 })
